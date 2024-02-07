3568983426200c3b9f2cfcfb0ad3e798ad52bca8
shehryar <shehryarkashan@gmail.com>
2024-02-07 17:19:21 +0500

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
 import "./App.css";
-import vid from "./assets/vid.gif";
+import { images } from "./assets";
+
 function App() {
   return (
-    <div class="img_containre">
-      <img src={vid} alt="gif" />
+    <div className="img_container">
+      <nav className="nav_bar">
+        <div className="brand">
+          <img src={images.logo} alt="logo" />
+        </div>
+        <ul className="right_side">
+          <li>
+            <a href="https://twitter.com/" without rel="noreferrer" target="_blank">
+              <img src={images.twitter} alt="twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="https://discord.com/" without rel="noreferrer" target="_blank">
+              <img src={images.discord} alt="discord" />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import logo from "./logo.png"
+import twitter from "./twitter.png"
+import discord from "./discord-icon.svg"
+
+export const images = {
+    logo,
+    twitter,
+    discord
+}
